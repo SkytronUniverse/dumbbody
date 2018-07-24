850abba6797932ad604339d6ccbffcb80521e655
SkytronUniverse <boatright.karl@gmail.com>
2018-07-24 16:21:36 -0400

    Preliminary Audio Tests

---
+++ DumbBody/Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour {
+
+    public AudioSource growl;
+    public AudioSource drums;
+
+    public void PlayAudioLoop()
+    {
+        growl.Play();
+        drums.Play();
+    }
+
+    public void StopAudioLoop()
+    {
+        growl.Stop();
+        drums.Stop();
+    }
+}
